f843e573203bed76cf9d2e34ab0a954247e7d74f
tarungautam137 <tarungautam137@gmail.com>
2024-09-21 18:38:42 +0530

    cookie consent

---
+++ cookie/script.js
@@ -0,0 +1,11 @@
+var div=document.querySelector("#second");
+var x=document.querySelector("#accept");
+var y=document.querySelector("#decline");
+
+x.addEventListener("click", function(){
+    div.style.display="none";
+});
+
+y.addEventListener("click", function(){
+    div.style.display="none";
+});
