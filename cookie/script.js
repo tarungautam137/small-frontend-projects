4ae9887ba1aca37f18e877b693467545f2513cce
tarungautam137 <tarungautam137@gmail.com>
2024-09-27 09:07:20 +0530

    Update script.js

--- cookie/script.js
+++ cookie/script.js
@@ -1,7 +1,6 @@
 var div=document.querySelector("#second");
 var accept=document.querySelector("#accept");
 var decline=document.querySelector("#decline");
-
 document.addEventListener("DOMContentLoaded",function() {
     if(!localStorage.getItem("accept")) div.style.display="block";
 });
